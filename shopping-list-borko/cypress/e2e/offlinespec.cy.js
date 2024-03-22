0c7f45372f99fa79304737578ed9771054a41c45
Daniel Lengsteiner <dlengsteiner@student.tgm.ac.at>
2024-03-22 11:02:18 +0100

    Add test case for offline sync

--- shopping-list-borko/cypress/e2e/offlinespec.cy.js
+++ shopping-list-borko/cypress/e2e/offlinespec.cy.js
@@ -2,7 +2,10 @@ describe('Check if Data is stored in PouchDB', () => {
   it('passes', () => {
     cy.visit('https://genuine-bublanina-389900.netlify.app/')
 
-    // Annahme: Nach dem Laden der Seite wird die Liste der gespeicherten Elemente angezeigt
+    cy.get('#settings').click()
+    cy.get('#syncUrl').type('https://apikey-v2-okuw20xktbmma1y05378zrc9eds44zp3rvrp3sxxckt:87b9177c31a91731a0e61f27fb3bf05b@746c698f-7d60-46e7-962a-d9efdd7c9a7d-bluemix.cloudantnosqldb.appdomain.cloud/shopping-list')
+    cy.get('#syncButton').click()
+    cy.get('#returnButton').click()
 
     cy.get('#itemlist').should('be.visible')
   })
